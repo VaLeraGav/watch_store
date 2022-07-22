78e29cfcbb6b26220c9281412ef1e91f62d1e9f1
VaLeraGav <deliciiouss@yandex.ru>
2022-07-22 14:41:29 +0300

    popular products

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -9,10 +9,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 <head>
 	<?= $this->getMeta(); ?>
-	<link href="/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 	<!--Custom-Theme-files-->
 	<!--theme-style-->
-	<link href="/css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 	<!--//theme-style-->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
